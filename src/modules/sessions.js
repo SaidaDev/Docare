6163d4692bb88a81367dee30fa9dfc90a5eb0f00
sadkoJs <w.wanderlust@gmail.com>
2021-11-13 18:41:34 +0400

    Use mock-fs in sessions test

--- src/modules/sessions.js
+++ src/modules/sessions.js
@@ -2,27 +2,24 @@ import fs from 'fs'
 import { getRandomString } from "./utilities"
 let sessions = {}
 
-const createSaveSessions = (fs) => () => {
+const saveSessions = () => {
     fs.writeFileSync('sessions.json', JSON.stringify(sessions, null, 2))
 }
 
-export const createLoadSession = (fs) => () => {
+export const loadSession = () => {
     sessions = JSON.parse(fs.readFileSync('sessions.json'))
 }
-
-export const loadSession = createLoadSession(fs)
 
 export const isSessionValid = (sessionId) => {
     return sessions[sessionId] !== undefined
 }
 
-export const createCloseSession = (fs) => (sessionId) => {
+export const closeSession = (sessionId) => {
     if (isSessionValid(sessionId)) {
         delete sessions[sessionId]
-        createSaveSessions(fs)()
+        saveSessions()
     }
 }
-export const closeSession = createCloseSession(fs)
 
 export const getDocId = (sessionId) => {
     const sessionobj = sessions[sessionId]
@@ -30,12 +27,12 @@ export const getDocId = (sessionId) => {
     return docId
 }
 
-export const createOpenSession = (save, getRandomString) => (docId) => {
+export const createOpenSession = (getRandomString) => (docId) => {
     const randomNum = getRandomString()
     sessions[randomNum] = {
         id: docId,
     }
-    save()
+    saveSessions()
     return randomNum
 }
-export const openSession = createOpenSession(createSaveSessions(fs), getRandomString)
+export const openSession = createOpenSession(getRandomString)
--- src/modules/sessions.test.js
+++ src/modules/sessions.test.js
@@ -1,37 +1,38 @@
-import { createCloseSession, createLoadSession, createOpenSession, getDocId, isSessionValid } from "./sessions";
-
-const loadSession = createLoadSession({
-    readFileSync: () =>
-        `{
+import { closeSession, createOpenSession, getDocId, isSessionValid, loadSession } from "./sessions";
+import mock from "mock-fs"
+describe("session test", () => {
+    beforeAll(() => {
+        mock({
+            "sessions.json": `{
         "38503": {"id": "0"}
         }`
-})
+        })
+        loadSession()
+    })
+    afterAll(() => {
+        mock.restore()
+    })
+    describe("isSessionValid", () => {
+        it("should return true", () => {
+            expect(isSessionValid("38503")).toBe(true)
+            expect(isSessionValid("12")).toBe(false)
+        })
+    })
 
-describe("isSessionValid", () => {
-    it("should return true", () => {
-        loadSession()
-        expect(isSessionValid("38503")).toBe(true)
-        expect(isSessionValid("12")).toBe(false)
+    describe("closeSession", () => {
+        it("should close session", () => {
+            closeSession("38503")
+            expect(isSessionValid("38503")).toBe(false)
+        })
     })
-})
+    describe("openSession", () => {
+        it("should open session", () => {
+            const randomStr = () => "5570"
+            const openSession = createOpenSession(randomStr)
+            openSession("0")
+            expect(isSessionValid("5570")).toBe(true)
+            expect(getDocId("5570")).toBe("0")
+        })
+    })
 
-describe("closeSession", () => {
-    it("should close session", () => {
-        const fsMine = { writeFileSync: () => { } }
-        const closeSession = createCloseSession(fsMine)
-        loadSession()
-        closeSession("38503")
-        expect(isSessionValid("38503")).toBe(false)
-    })
-})
-describe("openSession", () => {
-    it("should open session", () => {
-        const save = () => { }
-        const randomStr = () => "5570"
-        const openSession = createOpenSession(save, randomStr)
-        loadSession()
-        openSession("0")
-        expect(isSessionValid("5570")).toBe(true)
-        expect(getDocId("5570")).toBe("0")
-    })
 })
