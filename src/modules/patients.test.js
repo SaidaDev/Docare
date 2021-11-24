474d5ec1fb6e3000bcb245ae161e1deb153de8af
sadkoJs <w.wanderlust@gmail.com>
2021-11-24 22:09:45 +0400

    patient jest 2

--- src/modules/patients.test.js
+++ src/modules/patients.test.js
@@ -72,7 +72,10 @@ const patientsJson = `[{
     "phone": "+994 555 22 11",
     "doctorId": "0"
 }]`
-describe("patientsTest", () => {
+
+
+
+describe("getPatients", () => {
     beforeAll(() => {
         mock({
             "patients.json": patientsJson
@@ -82,37 +85,51 @@ describe("patientsTest", () => {
     afterAll(() => {
         mock.restore()
     })
+    it("should return patients", () => {
+        expect(getPatients("0", "", "", "", 1, 10)).toEqual([{
+            "image": "images/GYVC2191.JPG",
+            "id": "5656",
+            "name": "Alex Feinstein",
+            "age": 26,
+            "gender": "Male",
+            "disease": "Flu",
+            "treatment": 75,
+            "address": "Sweden",
+            "phone": "+176 154 45 25",
+            "doctorId": "0"
+        },
+        {
+            "image": "images/GYVC2191.JPG",
+            "id": "565",
+            "name": "Aygun Agakishibekova",
+            "age": 32,
+            "gender": "Female",
+            "disease": "heart attack",
+            "treatment": 35,
+            "address": "Baku",
+            "phone": "+994 555 22 11",
+            "doctorId": "0"
+        }])
+    })
 
-    describe("getPatients", () => {
-        it("should return patients", () => {
-            expect(getPatients("0", "", "", "", 1, 10)).toEqual([{
-                "image": "images/GYVC2191.JPG",
-                "id": "5656",
-                "name": "Alex Feinstein",
-                "age": 26,
-                "gender": "Male",
-                "disease": "Flu",
-                "treatment": 75,
-                "address": "Sweden",
-                "phone": "+176 154 45 25",
-                "doctorId": "0"
-            },
+    it("should return patients with search", () => {
+        expect(getPatients("1", "ilkin", "", "", 0, 10)).toEqual([{
+            "image": "images/GYVC2191.JPG",
+            "id": "2",
+            "name": "Ilkin Mamedov",
+            "age": 49,
+            "gender": "Male",
+            "disease": "Flu",
+            "treatment": 85,
+            "address": "Hannover",
+            "phone": "+176 450 54 50",
+            "doctorId": "1"
+        }])
+    })
+
+    it("should return patients with sort", () => {
+        expect(getPatients("1", "", "age", "d", 0, 10)).toEqual([
             {
-                "image": "images/GYVC2191.JPG",
-                "id": "565",
-                "name": "Aygun Agakishibekova",
-                "age": 32,
-                "gender": "Female",
-                "disease": "heart attack",
-                "treatment": 35,
-                "address": "Baku",
-                "phone": "+994 555 22 11",
-                "doctorId": "0"
-            }])
-        })
-
-        it("should return patients with search", () => {
-            expect(getPatients("1", "ilkin", "", "", 0, 10)).toEqual([{
                 "image": "images/GYVC2191.JPG",
                 "id": "2",
                 "name": "Ilkin Mamedov",
@@ -123,54 +140,47 @@ describe("patientsTest", () => {
                 "address": "Hannover",
                 "phone": "+176 450 54 50",
                 "doctorId": "1"
+            }, {
+                "image": "images/GYVC2191.JPG",
+                "id": "1",
+                "name": "Feride imanova",
+                "age": 42,
+                "gender": "Female",
+                "disease": "bronchitis",
+                "treatment": 37,
+                "address": "Gence",
+                "phone": "+276 460 54 50",
+                "doctorId": "1"
+            },
+            {
+                "image": "images/GYVC2191.JPG",
+                "id": "3",
+                "name": "Ilyas Aliyev",
+                "age": 32,
+                "gender": "Male",
+                "disease": "bronchitis",
+                "treatment": 65,
+                "address": "Hannover",
+                "phone": "+176 450 54 50",
+                "doctorId": "1"
             }])
-        })
-
-        it("should return patients with sort", () => {
-            expect(getPatients("1", "", "age", "d", 0, 10)).toEqual([
-                {
-                    "image": "images/GYVC2191.JPG",
-                    "id": "2",
-                    "name": "Ilkin Mamedov",
-                    "age": 49,
-                    "gender": "Male",
-                    "disease": "Flu",
-                    "treatment": 85,
-                    "address": "Hannover",
-                    "phone": "+176 450 54 50",
-                    "doctorId": "1"
-                }, {
-                    "image": "images/GYVC2191.JPG",
-                    "id": "1",
-                    "name": "Feride imanova",
-                    "age": 42,
-                    "gender": "Female",
-                    "disease": "bronchitis",
-                    "treatment": 37,
-                    "address": "Gence",
-                    "phone": "+276 460 54 50",
-                    "doctorId": "1"
-                },
-                {
-                    "image": "images/GYVC2191.JPG",
-                    "id": "3",
-                    "name": "Ilyas Aliyev",
-                    "age": 32,
-                    "gender": "Male",
-                    "disease": "bronchitis",
-                    "treatment": 65,
-                    "address": "Hannover",
-                    "phone": "+176 450 54 50",
-                    "doctorId": "1"
-                }])
-        })
-    })
-    describe("getNumPatients", () => {
-        it("should return patients for each doc", () => {
-            expect(getNumPatients("0")).toBe(3)
-        })
     })
 })
+describe("getNumPatients", () => {
+    beforeAll(() => {
+        mock({
+            "patients.json": patientsJson
+        })
+        loadPatients()
+    })
+    afterAll(() => {
+        mock.restore()
+    })
+    it("should return patients for each doc", () => {
+        expect(getNumPatients("0")).toBe(3)
+    })
+})
+
 describe("deletePatients", () => {
     beforeAll(() => {
         mock({
@@ -188,7 +198,7 @@ describe("deletePatients", () => {
         expect(getNumPatients("1")).toBe(1)
     })
 })
-describe("createAddPatients", () => {
+describe("AddPatients", () => {
     beforeAll(() => {
         mock({
             "patients.json": patientsJson
