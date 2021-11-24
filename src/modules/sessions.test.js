cd72ed68821765cf53c9511fc9a96b82dc76677e
sadkoJs <w.wanderlust@gmail.com>
2021-11-24 22:48:50 +0400

    Session Describe out

--- src/modules/sessions.test.js
+++ src/modules/sessions.test.js
@@ -1,6 +1,8 @@
 import { closeSession, getDocId, isSessionValid, loadSession, openSession } from "./sessions";
 import mock from "mock-fs"
-describe("session test", () => {
+
+ 
+describe("isSessionValid", () => {
     beforeAll(() => {
         mock({
             "sessions.json": `{
@@ -12,31 +14,41 @@ describe("session test", () => {
     afterAll(() => {
         mock.restore()
     })
-    describe("isSessionValid", () => {
-        it("should return true", () => {
-            expect(isSessionValid("38503")).toBe(true)
-            expect(isSessionValid("12")).toBe(false)
-        })
+    it("should return true", () => {
+        expect(isSessionValid("38503")).toBe(true)
+        expect(isSessionValid("12")).toBe(false)
     })
-
-    describe("closeSession", () => {
-        it("should close session", () => {
-            closeSession("38503")
-            expect(isSessionValid("38503")).toBe(false)
-        })
-    })
-    describe("openSession", () => {
-        beforeAll(() => {
-            jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
-        })
-        afterAll(() => {
-            Math.random.mockRestore();
-        })
-        it("should open session", () => {
-            openSession("0")
-            expect(isSessionValid("12345")).toBe(true)
-            expect(getDocId("12345")).toBe("0")
-        })
-    })
-
 })
+
+describe("closeSession", () => {
+    beforeAll(() => {
+        mock({
+            "sessions.json": `{
+        "38503": {"id": "0"}
+        }`
+        })
+        loadSession()
+    })
+    afterAll(() => {
+        mock.restore()
+    })
+    it("should close session", () => {
+        closeSession("38503")
+        expect(isSessionValid("38503")).toBe(false)
+    })
+})
+describe("openSession", () => {
+    beforeAll(() => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
+    })
+    afterAll(() => {
+        Math.random.mockRestore();
+    })
+    it("should open session", () => {
+        openSession("0")
+        expect(isSessionValid("12345")).toBe(true)
+        expect(getDocId("12345")).toBe("0")
+    })
+})
+
+
